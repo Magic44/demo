c26c23b9b2e912f6981a1e394758c7058c6afab7
Magic <2198719054@qq.com>
2023-03-18 18:04:14 +0800

    Second Commit

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,12 +1,22 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * @auther XXX
  * @data 2023/3/18
  * @apiNote
  */
+
+@Controller
 public class HelloController {
 
-
+    @ResponseBody
+    @RequestMapping("/")
+    public String hello(){
+        return "Hello World";
+    }
 
 }
